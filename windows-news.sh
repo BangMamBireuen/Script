4e1150ce49a20b7ebfbfe50623b7a86e34e2bdb7
BangMamBireuen <sebastian.imam130@gmail.com>
2025-03-04 14:24:55 +0700

    update testing

--- windows-news.sh
+++ windows-news.sh
@@ -1,58 +1,86 @@
 #!/bin/bash
 #
-# MODIFIKASI By BangMam (Deteksi Interface Jaringan Otomatis)
+# MODIFIKASI By BangMam (Password Otomatis untuk Opsi Tertentu)
 #
 
 echo "Pilih OS yang ingin anda install"
 echo "	1) Windows 2019 Password : Botol123456789!"
 echo "	2) Windows 2016 Password : Nixpoin.com123!"
 echo "	3) Windows 2012 Password : Nixpoin.com123!"
-echo "	4) Windows 10"
-echo "	5) Windows 2022"
+echo "	4) Windows 10 (Masukkan password manual)"
+echo "	5) Windows 2022 (Masukkan password manual)"
 echo "	6) Windows 2019 Password : P@ssword64"
 echo "	7) Pakai link gz mu sendiri"
 
 read -p "Pilih [1]: " PILIHOS
 
+# Tentukan link OS dan password default jika perlu
 case "$PILIHOS" in
-	1|"") PILIHOS="https://download1511.mediafire.com/w3qxhvst0hagzzCX8SQtNf_0UcPzp7unpfhJVu3_wtsV2pMiiEzJizjrJhb8EOYWkD1gO49Gfu9Vbx7Xplr8Mx1oAOUiV-NE3rrXV80YVB9imjT0NKXVFyJBR-hN00_lpFAzEkQTAwerg7ejmtwc7WCL5pVceuZswczjYWWbFrUA/oi1bb1p9heg6sbm/windows2019DO.gz";;
-	2) PILIHOS="https://download1503.mediafire.com/0ruxjt7yvpegs4u9I-4_WgHCaGvvVqdPTWAxJBM67Hc8zYX8VbKbn97J2NilZRLAwOgop-sKV8JdFmXb0hAU_OIUGhPGmTAxUJxxp2EC9zNM-U8yPTjoIaQTWTpfLmHu12z8Y02qaHJldGgJUJKitduvY76Tae8nbdq6NIsBtdjIPg/s8zxdghgha8m2wj/windows2016.gz";;
-	3) PILIHOS="https://download1349.mediafire.com/7e0d40pgxylg0suMFCA363KENgIe0cKuCWG7GRubeU9ROEmc-4wz2pgeaKyQCcPLb-q7I3Vn66pFJxX2uuf0wni5Hp5WB9viIkJnhm33MVbpaPfuq4YYZ1vV8HP0jXG0gjgdlvlUfpsCyUqT1isQTC2dRBaHMAusou30Ycrp3pXN/66rpxhj70pe3olc/windows2012v2.gz";;
-	4) PILIHOS="https://files.sowan.my.id/windows10.gz";;
-	5) PILIHOS="https://files.sowan.my.id/windows2022.gz";;
-	6) PILIHOS="https://download1349.mediafire.com/vi33u31onrsg56NlxqFTv6EsChol8dhGY-mU8Kqf0AHReK5h4DOhwOWvFJTTPUiWbYl0JmqYneEs_iWSTqxn2FMq2Dll805G1SYwfA7yIU2M1rA3rqmXuWOxIs73SwMZjTMRzu1G8-zoa-rNBdSpGtW4bNHau42zRhjpS5KaZjep2nw/r0h9kuzoxq7rp19/windows19.gz";;
-	7) read -p "Masukkan Link GZ mu : " PILIHOS;;
-	*) echo "Pilihan salah"; exit;;
+	1|"") 
+		PILIHOS="https://download1511.mediafire.com/w3qxhvst0hagzzCX8SQtNf_0UcPzp7unpfhJVu3_wtsV2pMiiEzJizjrJhb8EOYWkD1gO49Gfu9Vbx7Xplr8Mx1oAOUiV-NE3rrXV80YVB9imjT0NKXVFyJBR-hN00_lpFAzEkQTAwerg7ejmtwc7WCL5pVceuZswczjYWWbFrUA/oi1bb1p9heg6sbm/windows2019DO.gz"
+		PASSADMIN="Botol123456789!"
+		;;
+	2) 
+		PILIHOS="https://download1503.mediafire.com/0ruxjt7yvpegs4u9I-4_WgHCaGvvVqdPTWAxJBM67Hc8zYX8VbKbn97J2NilZRLAwOgop-sKV8JdFmXb0hAU_OIUGhPGmTAxUJxxp2EC9zNM-U8yPTjoIaQTWTpfLmHu12z8Y02qaHJldGgJUJKitduvY76Tae8nbdq6NIsBtdjIPg/s8zxdghgha8m2wj/windows2016.gz"
+		PASSADMIN="Nixpoin.com123!"
+		;;
+	3) 
+		PILIHOS="https://download1349.mediafire.com/7e0d40pgxylg0suMFCA363KENgIe0cKuCWG7GRubeU9ROEmc-4wz2pgeaKyQCcPLb-q7I3Vn66pFJxX2uuf0wni5Hp5WB9viIkJnhm33MVbpaPfuq4YYZ1vV8HP0jXG0gjgdlvlUfpsCyUqT1isQTC2dRBaHMAusou30Ycrp3pXN/66rpxhj70pe3olc/windows2012v2.gz"
+		PASSADMIN="Nixpoin.com123!"
+		;;
+	4) 
+		PILIHOS="https://files.sowan.my.id/windows10.gz"
+		read -p "Masukkan password untuk akun Administrator: " PASSADMIN
+		;;
+	5) 
+		PILIHOS="https://files.sowan.my.id/windows2022.gz"
+		read -p "Masukkan password untuk akun Administrator: " PASSADMIN
+		;;
+	6) 
+		PILIHOS="https://download1349.mediafire.com/vi33u31onrsg56NlxqFTv6EsChol8dhGY-mU8Kqf0AHReK5h4DOhwOWvFJTTPUiWbYl0JmqYneEs_iWSTqxn2FMq2Dll805G1SYwfA7yIU2M1rA3rqmXuWOxIs73SwMZjTMRzu1G8-zoa-rNBdSpGtW4bNHau42zRhjpS5KaZjep2nw/r0h9kuzoxq7rp19/windows19.gz"
+		PASSADMIN="P@ssword64"
+		;;
+	7) 
+		read -p "Masukkan Link GZ mu : " PILIHOS
+		read -p "Masukkan password untuk akun Administrator: " PASSADMIN
+		;;
+	*) 
+		echo "Pilihan salah"
+		exit
+		;;
 esac
 
 echo "Gunakan script ini dengan bijak, jika ada masalah hubungi WA Admin 083117542926"
 
-read -p "Masukkan password untuk akun Administrator: " PASSADMIN
-
-# Menunggu koneksi internet sebelum lanjut
+# Tunggu koneksi internet sebelum lanjut
 while ! ping -c 1 8.8.8.8 &>/dev/null; do
     echo "Menunggu koneksi internet..."
     sleep 5
 done
 
-# Ambil IP Publik (pakai metode alternatif jika gagal)
+# Ambil IP Publik
 IP4=$(wget -qO- icanhazip.com || curl -s https://api64.ipify.org)
 
-# Ambil Default Gateway (pakai metode alternatif jika gagal)
+# Ambil Default Gateway
 GW=$(ip route | grep default | awk '{print $3}')
 if [[ -z "$GW" ]]; then
     GW=$(route -n | awk '/UG/ {print $2}')
 fi
 
 # **Deteksi Interface Secara Otomatis**
-IFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -E "Ethernet|Instance" | head -n 1)
+IFACE=$(ip route | grep default | awk '{print $5}')  # Cek interface dari default route
 
-# **Jika lebih dari satu interface, pilih yang memiliki koneksi aktif**
-if [[ $(echo "$IFACE" | wc -l) -gt 1 ]]; then
-    IFACE=$(ip -4 route ls | grep default | awk '{print $5}')
+# Jika tidak terdeteksi, cari interface yang memiliki IP (bukan loopback)
+if [[ -z "$IFACE" ]]; then
+    IFACE=$(ip -o -4 addr show | grep -v "127.0.0.1" | awk '{print $2}' | head -n 1)
 fi
 
-# **Jika masih kosong, pakai default "Ethernet"**
+# Jika masih kosong, coba cari yang mengandung "Ethernet" atau "Instance"
+if [[ -z "$IFACE" ]]; then
+    IFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -E "Ethernet|Instance" | head -n 1)
+fi
+
+# Jika masih kosong, pakai default "Ethernet"
 if [[ -z "$IFACE" ]]; then
     IFACE="Ethernet"
 fi
