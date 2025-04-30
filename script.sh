bc3b533115223e19ccf4175e53342dc37aaec156
BangMamBireuen <sebastian.imam130@gmail.com>
2025-04-30 22:52:58 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,15 +2,33 @@
 #
 # CREATED By NIXPOIN.COM
 #
-echo "Installing Windows 2016..."
-PILIHOS="https://download1503.mediafire.com/sg1dzn7yyiagwQSWD97oAve0wSjPI-5Jpb-BF1LIUOf-Vgd_IHmUj7IB-dmQZeNhUETanxl7duVcmfkI2GnkyLa1UKW4ziDxTplDztRx1Zd0wlu4-SslfULf8NRxeD3L46Z8wloskorwsfFtAxmWNPRG7DoEcKlRMwydpZGIEEmX/s8zxdghgha8m2wj/windows2016.gz"
-IFACE="Ethernet Instance 0 2"
-PASSADMIN="Nixpoin.com123!"
+echo "Pilih OS yang ingin anda install"
+echo "	1) Windows 2019(Default)"
+echo "	2) Windows 2016"
+echo "	3) Windows 2012"
+echo "	4) Windows 10"
+echo "	5) Windows 2022"
+echo "	6) Pakai link gz mu sendiri"
+
+read -p "Pilih [1]: " PILIHOS
+
+case "$PILIHOS" in
+	1|"") PILIHOS="https://files.sowan.my.id/windows2019.gz"  IFACE="Ethernet Instance 0 2";;
+	2) PILIHOS="https://download1503.mediafire.com/sg1dzn7yyiagwQSWD97oAve0wSjPI-5Jpb-BF1LIUOf-Vgd_IHmUj7IB-dmQZeNhUETanxl7duVcmfkI2GnkyLa1UKW4ziDxTplDztRx1Zd0wlu4-SslfULf8NRxeD3L46Z8wloskorwsfFtAxmWNPRG7DoEcKlRMwydpZGIEEmX/s8zxdghgha8m2wj/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
+	3) PILIHOS="https://files.sowan.my.id/windows2012.gz"  IFACE="Ethernet";;
+	4) PILIHOS="https://files.sowan.my.id/windows10.gz"  IFACE="Ethernet Instance 0 2";;
+	5) PILIHOS="https://files.sowan.my.id/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
+	6) read -p "Masukkan Link GZ mu : " PILIHOS;;
+	*) echo "pilihan salah"; exit;;
+esac
 
 echo "Merasa terbantu dengan script ini? Anda bisa memberikan dukungan melalui QRIS kami https://nixpoin.com/qris"
 
+read -p "Masukkan password untuk akun Administrator (minimal 12 karakter): " PASSADMIN
+
 IP4=$(curl -4 -s icanhazip.com)
 GW=$(ip route | awk '/default/ { print $3 }')
+
 
 cat >/tmp/net.bat<<EOF
 @ECHO OFF
@@ -22,6 +40,7 @@ del /f /q "%temp%\Admin.vbs"
 exit /b 2)
 net user Administrator $PASSADMIN
 
+
 netsh -c interface ip set address name="$IFACE" source=static address=$IP4 mask=255.255.240.0 gateway=$GW
 netsh -c interface ip add dnsservers name="$IFACE" address=1.1.1.1 index=1 validate=no
 netsh -c interface ip add dnsservers name="$IFACE" address=8.8.4.4 index=2 validate=no
@@ -30,6 +49,7 @@ cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
 del /f /q net.bat
 exit
 EOF
+
 
 cat >/tmp/dpart.bat<<EOF
 @ECHO OFF
