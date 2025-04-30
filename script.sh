c76818b167f4559def6b09ea30cea1f29d35d042
BangMamBireuen <sebastian.imam130@gmail.com>
2025-04-30 23:31:47 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "	6) Pakai link gz mu sendiri"
 read -p "Pilih [1]: " PILIHOS
 
 case "$PILIHOS" in
-	1|"") PILIHOS="https://download1511.mediafire.com/on3f7r6sflkgYbvm5pfyugP6WCqZtkKDOGyZiVFV3H292NR2mkFN-AGf_GsWQZ7SrA4EcKvewcM8tqr1Mo_oFo0irOibdSaM4nU49q_-GD-5rHY7E1foTDl09nWbT5w7K9tlyW1tpBA5wxOha4yHwr6vyZYGfVt5DsQFHsmJTPXj/oi1bb1p9heg6sbm/windows2019DO.gz"  IFACE="Ethernet Instance 0 2";;
+	1|"") PILIHOS="https://download1511.mediafire.com/on3f7r6sflkgYbvm5pfyugP6WCqZtkKDOGyZiVFV3H292NR2mkFN-AGf_GsWQZ7SrA4EcKvewcM8tqr1Mo_oFo0irOibdSaM4nU49q_-GD-5rHY7E1foTDl09nWbT5w7K9tlyW1tpBA5wxOha4yHwr6vyZYGfVt5DsQFHsmJTPXj/oi1bb1p9heg6sbm/windows2019DO.gz"  IFACE="Ethernet Instance 0";;
 	2) PILIHOS="https://files.sowan.my.id/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
 	3) PILIHOS="https://files.sowan.my.id/windows2012.gz"  IFACE="Ethernet";;
 	4) PILIHOS="https://files.sowan.my.id/windows10.gz"  IFACE="Ethernet Instance 0 2";;
