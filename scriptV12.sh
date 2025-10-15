96891f972bf3b4e92a5fc5050f75162d489f0a64
BangMamBireuen <sebastian.imam130@gmail.com>
2025-10-15 10:04:54 +0700

    Update and rename scriptV11.sh to scriptV12.sh

--- scriptV12.sh
+++ scriptV12.sh
@@ -40,7 +40,7 @@ EOF
 cat >/tmp/dpart.bat<<EOF
 @ECHO OFF
 echo JENDELA INI JANGAN DITUTUP
-echo SCRIPT INI AKAN MERUBAH PORT RDP MENJADI 5000, SETELAH RESTART UNTUK MENYAMBUNG KE RDP GUNAKAN ALAMAT $IP4:5000
+echo Port RDP diubah ke 5000. Koneksi via $IP4:5000 setelah restart.
 echo PROSES AKAN BERJALAN OTOMATIS DALAM 5 DETIK...
 
 cd.>%windir%\GetAdmin
@@ -73,141 +73,45 @@ del /f /q "%SystemDrive%\diskpart.extend"
 
 :: Download dan install Chrome
 echo Mengunduh Chrome...
-powershell -Command "& {
-    `$url = 'https://dl.google.com/chrome/install/latest/chrome_installer.exe'
-    `$output = '%TEMP%\ChromeInstaller.exe'
-    `$progressPreference = 'silentlyContinue'
-    Invoke-WebRequest -Uri `$url -OutFile `$output
-    Write-Host 'Download Chrome selesai'
-}"
+powershell -Command "Invoke-WebRequest -Uri 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile '%TEMP%\ChromeInstaller.exe'"
 echo Menginstall Chrome...
 %TEMP%\ChromeInstaller.exe /silent /install
 echo Menghapus installer Chrome...
 del /f /q "%TEMP%\ChromeInstaller.exe"
 
-:: Download dan install PostgreSQL 9.4.26 dengan progress
+:: Download dan install PostgreSQL 9.4.26
 echo Mengunduh PostgreSQL 9.4.26...
-powershell -Command "& {
-    `$url = 'https://get.enterprisedb.com/postgresql/postgresql-9.4.26-1-windows-x64.exe'
-    `$output = '%TEMP%\postgresql-9.4.26.exe'
-    `$progressPreference = 'silentlyContinue'
-    
-    Write-Host 'Memulai download PostgreSQL 9.4.26...'
-    `$startTime = Get-Date
-    Invoke-WebRequest -Uri `$url -OutFile `$output
-    `$endTime = Get-Date
-    `$fileSize = (Get-Item `$output).Length
-    `$downloadTime = (`$endTime - `$startTime).TotalSeconds
-    `$speed = `$fileSize / `$downloadTime / 1MB
-    
-    Write-Host "Download PostgreSQL selesai!"
-    Write-Host "Ukuran file: [`$([math]::Round(`$fileSize/1MB, 2)) MB]"
-    Write-Host "Waktu download: [`$([math]::Round(`$downloadTime, 2)) detik]"
-    Write-Host "Kecepatan rata-rata: [`$([math]::Round(`$speed, 2)) MB/s]"
-}"
+powershell -Command "Invoke-WebRequest -Uri 'https://get.enterprisedb.com/postgresql/postgresql-9.4.26-1-windows-x64.exe' -OutFile '%TEMP%\postgresql-9.4.26.exe'"
 echo Menginstall PostgreSQL 9.4.26...
 echo Proses instalasi PostgreSQL akan dimulai...
-echo Harap tunggu, proses mungkin memakan waktu beberapa menit...
-%TEMP%\postgresql-9.4.26.exe --unattendedmodeui none --superpassword "123456" --servicename "PostgreSQL" --servicepassword "123456" --serverport 5432
-if %ERRORLEVEL% EQU 0 (
-    echo PostgreSQL berhasil diinstall!
-) else (
-    echo Terjadi error saat install PostgreSQL
-)
+start /wait "" "%TEMP%\postgresql-9.4.26.exe" --mode unattended --superpassword "123456" --servicename "PostgreSQL" --servicepassword "123456" --serverport 5432
 echo Menghapus installer PostgreSQL...
 del /f /q "%TEMP%\postgresql-9.4.26.exe"
 
-:: Download dan install XAMPP 7.4.29 dengan progress
+:: Download dan install XAMPP 7.4.29
 echo Mengunduh XAMPP 7.4.29...
-powershell -Command "& {
-    `$url = 'https://onboardcloud.dl.sourceforge.net/project/xampp/XAMPP%20Windows/7.4.29/xampp-windows-x64-7.4.29-0-VC15-installer.exe?viasf=1'
-    `$output = '%TEMP%\xampp-installer.exe'
-    `$progressPreference = 'silentlyContinue'
-    
-    Write-Host 'Memulai download XAMPP 7.4.29...'
-    `$startTime = Get-Date
-    Invoke-WebRequest -Uri `$url -OutFile `$output
-    `$endTime = Get-Date
-    `$fileSize = (Get-Item `$output).Length
-    `$downloadTime = (`$endTime - `$startTime).TotalSeconds
-    `$speed = `$fileSize / `$downloadTime / 1MB
-    
-    Write-Host "Download XAMPP selesai!"
-    Write-Host "Ukuran file: [`$([math]::Round(`$fileSize/1MB, 2)) MB]"
-    Write-Host "Waktu download: [`$([math]::Round(`$downloadTime, 2)) detik]"
-    Write-Host "Kecepatan rata-rata: [`$([math]::Round(`$speed, 2)) MB/s]"
-}"
+powershell -Command "Invoke-WebRequest -Uri 'https://onboardcloud.dl.sourceforge.net/project/xampp/XAMPP%20Windows/7.4.29/xampp-windows-x64-7.4.29-0-VC15-installer.exe?viasf=1' -OutFile '%TEMP%\xampp-installer.exe'"
 echo Menginstall XAMPP 7.4.29...
 echo Proses instalasi XAMPP akan dimulai...
-echo Harap tunggu, proses mungkin memakan waktu beberapa menit...
-%TEMP%\xampp-installer.exe /S
-if %ERRORLEVEL% EQU 0 (
-    echo XAMPP berhasil diinstall!
-) else (
-    echo Terjadi error saat install XAMPP
-)
+start /wait "" "%TEMP%\xampp-installer.exe" /S
 echo Menghapus installer XAMPP...
 del /f /q "%TEMP%\xampp-installer.exe"
 
-:: Download dan install Notepad++ 7.8.5 dengan progress
+:: Download dan install Notepad++ 7.8.5
 echo Mengunduh Notepad++ 7.8.5...
-powershell -Command "& {
-    `$url = 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.5/npp.7.8.5.Installer.x64.exe'
-    `$output = '%TEMP%\notepadplusplus-installer.exe'
-    `$progressPreference = 'silentlyContinue'
-    
-    Write-Host 'Memulai download Notepad++ 7.8.5...'
-    `$startTime = Get-Date
-    Invoke-WebRequest -Uri `$url -OutFile `$output
-    `$endTime = Get-Date
-    `$fileSize = (Get-Item `$output).Length
-    `$downloadTime = (`$endTime - `$startTime).TotalSeconds
-    `$speed = `$fileSize / `$downloadTime / 1MB
-    
-    Write-Host "Download Notepad++ selesai!"
-    Write-Host "Ukuran file: [`$([math]::Round(`$fileSize/1MB, 2)) MB]"
-    Write-Host "Waktu download: [`$([math]::Round(`$downloadTime, 2)) detik]"
-    Write-Host "Kecepatan rata-rata: [`$([math]::Round(`$speed, 2)) MB/s]"
-}"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.5/npp.7.8.5.Installer.x64.exe' -OutFile '%TEMP%\notepadplusplus-installer.exe'"
 echo Menginstall Notepad++ 7.8.5...
 echo Proses instalasi Notepad++ akan dimulai...
 %TEMP%\notepadplusplus-installer.exe /S
-if %ERRORLEVEL% EQU 0 (
-    echo Notepad++ berhasil diinstall!
-) else (
-    echo Terjadi error saat install Notepad++
-)
 echo Menghapus installer Notepad++...
 del /f /q "%TEMP%\notepadplusplus-installer.exe"
 
-:: Download dan install WinRAR 7.13 dengan progress
+:: Download dan install WinRAR 7.13
 echo Mengunduh WinRAR 7.13...
-powershell -Command "& {
-    `$url = 'https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-713.exe'
-    `$output = '%TEMP%\winrar-installer.exe'
-    `$progressPreference = 'silentlyContinue'
-    
-    Write-Host 'Memulai download WinRAR 7.13...'
-    `$startTime = Get-Date
-    Invoke-WebRequest -Uri `$url -OutFile `$output
-    `$endTime = Get-Date
-    `$fileSize = (Get-Item `$output).Length
-    `$downloadTime = (`$endTime - `$startTime).TotalSeconds
-    `$speed = `$fileSize / `$downloadTime / 1MB
-    
-    Write-Host "Download WinRAR selesai!"
-    Write-Host "Ukuran file: [`$([math]::Round(`$fileSize/1MB, 2)) MB]"
-    Write-Host "Waktu download: [`$([math]::Round(`$downloadTime, 2)) detik]"
-    Write-Host "Kecepatan rata-rata: [`$([math]::Round(`$speed, 2)) MB/s]"
-}"
+powershell -Command "Invoke-WebRequest -Uri 'https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-713.exe' -OutFile '%TEMP%\winrar-installer.exe'"
 echo Menginstall WinRAR 7.13...
 echo Proses instalasi WinRAR akan dimulai...
 %TEMP%\winrar-installer.exe /S
-if %ERRORLEVEL% EQU 0 (
-    echo WinRAR berhasil diinstall!
-) else (
-    echo Terjadi error saat install WinRAR
-)
 echo Menghapus installer WinRAR...
 del /f /q "%TEMP%\winrar-installer.exe"
 
@@ -223,15 +127,6 @@ if exist ChromeSetup3.exe del /f /q ChromeSetup3.exe
 cd /d "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup"
 del /f /q dpart.bat
 echo JENDELA INI JANGAN DITUTUP
-echo ========================================
-echo SEMUA APLIKASI TELAH BERHASIL DIINSTALL!
-echo ========================================
-echo Chrome - Browser web
-echo PostgreSQL 9.4.26 - Database server
-echo XAMPP 7.4.29 - Web server stack
-echo Notepad++ 7.8.5 - Text editor
-echo WinRAR 7.13 - File archiver
-echo ========================================
 exit
 EOF
 
