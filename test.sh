e2e5796f2034e3caec47e6e08cc65ee3c6cf074f
BangMamBireuen <sebastian.imam130@gmail.com>
2025-10-23 10:55:39 +0700

    Update Xampp Installer

--- test.sh
+++ test.sh
@@ -173,9 +173,9 @@ if exist "C:\installers\postgresql-installer.exe" (
     echo [GAGAL] ERROR: PostgreSQL installer tidak ditemukan!
 )
 
-:: Install XAMPP 8.1.25
+:: Install XAMPP 7.4.30-1
 echo.
-echo [4/8] Menginstall XAMPP 8.1.25...
+echo [4/6] Menginstall XAMPP 7.4.30-1...
 if exist "C:\installers\xampp-installer.exe" (
     echo Memulai instalasi XAMPP...
     start /wait "" "C:\installers\xampp-installer.exe" --mode unattended --unattendedmodeui minimal --installer-language en --prefix "C:\xampp"    
@@ -184,7 +184,6 @@ if exist "C:\installers\xampp-installer.exe" (
     :: Verifikasi apakah instalasi berhasil
     if exist "C:\xampp\xampp-control.exe" (
         echo [BERHASIL] XAMPP berhasil diinstall di C:\xampp
-        :: TIDAK memanggil start xampp-control.exe seperti sebelumnya
     ) else (
         echo [GAGAL] XAMPP gagal terinstall - file kontrol tidak ditemukan
         echo [INFO] Mencoba verifikasi alternatif...
