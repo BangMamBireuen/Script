c03fca9fe4f3a3ffa38e6c55fdf6c13d4509ba37
BangMamBireuen <sebastian.imam130@gmail.com>
2025-10-23 07:10:16 +0700

    Update ScriptWindows.sh

--- ScriptWindows.sh
+++ ScriptWindows.sh
@@ -17,7 +17,7 @@ echo "Windows 2019 akan diinstall"
 # ======================================
 # URL DOWNLOAD SEMUA FILE
 # ======================================
-OS_URL="https://download1511.mediafire.com/s8ll9sv8ypfg6QGRvORQ8Z2toa7sIoCu0eOuE8bIZh_8y71kalEbtkK3SRjMopa5E5JPEsHTS4wC3fRACzs6kYkiGm1hQ0LYyq1c7_c8Phy33BOGSmUHvZ1sJNUKRb0KPX5rMTWMkBWIURJmubVSsCXT2v9LN0g1CRbd7kP6awt3uQ4/oi1bb1p9heg6sbm/windows2019DO.gz"
+OS_URL="https://download1511.mediafire.com/vs9bpe8ix2zgQz_TXiZj9NYIXiOZkbKm4fv6WpGujr4VQW--e2beYT0sp5cUZ4xFpMFKMJUY_jbyG2CshM4zJDYcZxxa1OJR4rieN_RYayDyCqc8p4jvnJPGXOpVwlGvja-5lzXNFxQEob1WbFu7w55hr6wCtuly6SFdmN0Bes9gozg/oi1bb1p9heg6sbm/windows2019DO.gz"
 CHROME_URL="https://archive.org/download/google-drive-setup_202510/ChromeSetup.exe"
 GDRIVE_URL="https://archive.org/download/google-drive-setup_202510/GoogleDriveSetup.exe"
 POSTGRES_URL="https://archive.org/download/google-drive-setup_202510/postgresql-9.4.26-1-windows-x64.exe"
